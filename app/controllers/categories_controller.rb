29bf2066cd9e0d0de4bb4400721fb9b80aa317f8
Chris Bastian <peacethrubeats@gmail.com>
2015-05-16 22:24:22 -0700

    edit, destroy books done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,7 +28,9 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
     @categories = Category.all
+    @books = @category.books
   end
 
   private
