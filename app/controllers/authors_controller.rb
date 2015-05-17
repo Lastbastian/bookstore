ae077bf952e70c0516d61b8c7625de1e5ad219b7
Chris Bastian <peacethrubeats@gmail.com>
2015-05-17 12:28:21 -0700

    finished edit, update, delete on publishers, authors, categories.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -15,18 +15,28 @@ class AuthorsController < ApplicationController
   end
 
   def update
+    @author = Author.find(params[:id])
   end
 
   def edit
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+
+    flash[:notice] = "Author Updated"
+    redirect_to authors_path
   end
 
   def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    flash[:notice] = 'Author Deleted'
+
+    redirect_to authors_path
   end
 
   def index
-  end
-
-  def show
+    @authors = Author.all
   end
 
   private
